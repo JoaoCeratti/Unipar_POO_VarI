8464af4966997b114a03cda23cbfca62aae52476
JoaoCeratti <160682434+JoaoCeratti@users.noreply.github.com>
2024-02-20 22:35:50 -0300

    Create Atividade1.java

---
+++ Atividade1.java
@@ -0,0 +1,20 @@
+
+class HelloWorld {
+    public static void main(String[] args) {
+      
+      
+  String name = "jv";
+  short age = 19;
+  double n1 = 10;
+  double n2 = 5;
+  double media = (n1 + n2) / 2;
+  int aprovate = 6;
+  int reprovate = 6;
+  
+  
+  
+  System.out.println(name + " tem uma media de " + media +"\n");
+  System.out.println("caso a media for >=" + aprovate + ", o aluno esta aprovado");
+  System .out.println("caso a media for <" + reprovate + ", o aluno esta reprovado");
+    }
+}
